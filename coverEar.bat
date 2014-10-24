d321d4bfa01c078ff4819bd34f1f6b3b4502c284
Cloud <biluncloud@gmail.com>
2014-10-24 11:37:06 +0800

    Use the file list to copy, if failed, store into another list

--- coverEar.bat
+++ coverEar.bat
@@ -28,14 +28,23 @@ goto :L_CONFIG_LOG
     if "%TEMP%" EQU "" (
         set ERROR_LOG=coverEar.err
         set NORMAL_LOG=coverEar.log
-        set FILE_LIST=coverEar.lst
+        set FILE_LIST=coverEar_file.lst
+        set FAILED_FILE_LIST=coverEar_failed.lst
         set TEMP_FILE=coverEar.tmp
     ) else (
         set ERROR_LOG=%TEMP%\coverEar.err
         set NORMAL_LOG=%TEMP%\coverEar.log
-        set FILE_LIST=%TEMP%\coverEar.lst
+        set FILE_LIST=%TEMP%\coverEar_file.lst
+        set FAILED_FILE_LIST=coverEar_failed.lst
         set TEMP_FILE=%TEMP%\coverEar.tmp
     )
+
+    rem Clean the list file first
+    if exist %ERROR_LOG%        del %ERROR_LOG%
+    if exist %NORMAL_LOG%       del %NORMAL_LOG%
+    if exist %FILE_LIST%        del %FILE_LIST%
+    if exist %FAILED_FILE_LIST% del %FAILED_FILE_LIST%
+    if exist %TEMP_FILE%        del %TEMP_FILE%
 
 :L_EVAL_PARAM
     shift
@@ -69,36 +78,38 @@ goto :L_CONFIG_LOG
         echo Can not find: %SOURCE%
         echo Please check whether the source path is right.
         goto :L_HELP
-    ) else (
-        rem Store all the files into a list
-        rem The way to get the directory of the source, it's based
-        rem on the output of dir command.
-        for /f "tokens=3" %%i in ('"dir %SOURCE% | findstr "Directory of" "') do set FOLDER=%i\
-        rem Clean the list file first
-        echo. > %FILE_LIST%
-        for /f %%i in ('dir /B %SOURCE%') do echo %FOLDER%%%i >> %FILE_LIST%
     )
+    rem Store all the files into a list
+    rem The way to get the directory of the source, it's based
+    rem on the output of dir command.
+    for /f "tokens=3" %%i in ('"dir %SOURCE% | findstr "Directory of" "') do set FOLDER=%%i\
+    for /f %%i in ('dir /B %SOURCE%') do (echo !FOLDER!%%i >> %FILE_LIST%)
 
     if "%DEST%" EQU "" goto :L_HELP
     rem Default retry is 10.
     if "%RETRY%" EQU "" set RETRY=10
 
-    set /A COUNT=1
 :L_START_COMMAND
     rem for could not be used here because we need to check the 
     rem errorlevel everytime, however if for is used, errorlevel
     rem would be expanded at the very early stage and the loop 
     rem would continue whatever the copy command succeeded or not.
-    echo call copy /Z %SOURCE% %DEST% 2>%ERROR_LOG%
-    cd sdagsdag
-    if %errorlevel% EQU 0 goto :L_EXIT
-    if !COUNT! NEQ %RETRY% (
-        echo Retrying ... !COUNT!
-        set /A COUNT+=1
-        goto :L_START_COMMAND
-    ) else (
-        echo Failed to copy from %SOUCE% to %DEST% for %RETRY% times.
-        goto :L_EXIT
+    for /f %%i in (%FILE_LIST%) do (
+        echo %%i
+
+        set /A COUNT=1
+:L_START_COPY_ONE
+        echo call copy /Z %%i %DEST% 2>%ERROR_LOG%
+        cd sdagsdag
+        if !errorlevel! EQU 0 goto :L_EXIT
+        if !COUNT! NEQ %RETRY% (
+            echo Retrying ... !COUNT!
+            set /A COUNT+=1
+            goto :L_START_COPY_ONE
+        ) else (
+            echo Failed to copy from %%i to %DEST% for %RETRY% times.
+            echo %%i >> %FAILED_FILE_LIST%
+        )
     )
 
 :L_EXIT
@@ -110,6 +121,7 @@ goto :L_CONFIG_LOG
             rem call del %ERROR_LOG%
             rem call del %NORMAL_LOG%
             rem call del %FILE_LIST%
+            rem call del %FAILED_FILE_LIST%
             rem call del %TEMP_FILE%
         )
     )
